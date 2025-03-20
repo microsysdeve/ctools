d05f8d6cf1aad6c73223f510f9b81cb896732dad
microsys <microsys@sina.com>
2025-03-20 22:36:27 +0800

    添加了astyle

--- clearfile.bat
+++ clearfile.bat
@@ -1,4 +1,15 @@
-@echo off
-set srcDir="f:\cache"
+REM  @echo off
+
+set srcDir="C:\Users\micro\Documents\WeChat Files\microsys\FileStorage\Video"
+set daysAgo=90
+forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
+ 
+set srcDir="d:\cache"
 set daysAgo=10
-forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path"
+forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path /q"
+forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
+
+set srcDir="Z:\netcache"
+set daysAgo=30
+forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path /q"
+forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
