4960df1a8f691a6baa5fc2a4a80749be518b2e9e
microsys <microsys@sina.com>
2025-10-05 15:31:01 +0800

    23

--- clearfile.bat
+++ clearfile.bat
@@ -4,7 +4,7 @@ set srcDir="C:\Users\micro\Documents\WeChat Files\microsys\FileStorage\Video"
 set daysAgo=90
 forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
  
-set srcDir="d:\cache"
+set srcDir="E:\cache"
 set daysAgo=10
 forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path /q"
 forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
--- delrecycle.bat
+++ delrecycle.bat
@@ -1,11 +1,13 @@
-forfiles /p d:\cache /d -10 /s /m *.* /c "cmd /c del /q /f @path"
+forfiles /p e:\cache /d -10 /s /m *.* /c "cmd /c del /q /f @path"
 
-forfiles /p d:\$recycle.bin  /s /m *.o /c "cmd /c del /q /f @path"
 forfiles /p c:\$recycle.bin  /s /m *.o /c "cmd /c del /q /f @path"
+forfiles /p d:\$recycle.bin  /s /m *.o /c "cmd /c del /q /f @path"
+forfiles /p e:\$recycle.bin  /s /m *.o /c "cmd /c del /q /f @path"
 
-forfiles /p d:\$recycle.bin  /s /m *.tmp /c "cmd /c del /q /f @path"
 forfiles /p c:\$recycle.bin  /s /m *.tmp /c "cmd /c del /q /f @path"
+forfiles /p d:\$recycle.bin  /s /m *.tmp /c "cmd /c del /q /f @path"
+forfiles /p e:\$recycle.bin  /s /m *.tmp /c "cmd /c del /q /f @path"
 
-
-forfiles /p d:\$recycle.bin /d -10 /s /m *.* /c "cmd /c del /q /f @path"
 forfiles /p c:\$recycle.bin /d -10 /s /m *.* /c "cmd /c del /q /f @path"
+forfiles /p d:\$recycle.bin /d -10 /s /m *.* /c "cmd /c del /q /f @path"
+forfiles /p e:\$recycle.bin /d -10 /s /m *.* /c "cmd /c del /q /f @path"
