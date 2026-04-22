c28f3c01a4c7eb502544bc9f8575b373f2487981
microsys@sina.com <microsys@sina.com>
2026-04-22 14:41:18 +0800

    添加了srec_cat.exe

--- clearfile.bat
+++ clearfile.bat
@@ -1,15 +1,12 @@
 REM  @echo off
 
-set srcDir="C:\Users\micro\Documents\WeChat Files\microsys\FileStorage\Video"
-set daysAgo=90
-forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
+REM set srcDir="C:\Users\micro\Documents\WeChat Files\microsys\FileStorage\Video"
+REM set daysAgo=90
+REM forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
  
-set srcDir="E:\cache"
+set srcDir="D:\cache"
 set daysAgo=10
 forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path /q"
 forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
 
-set srcDir="Z:\netcache"
-set daysAgo=30
-forfiles /p %srcDir% /s /m *.dat  /d -%daysAgo% /c "cmd /c del @path /q"
-forfiles /p %srcDir% /s /m *.*  /d -%daysAgo% /c "cmd /c del @path /q"
+ 
